d344b40d0a4fe451f834ff37cde60900e9346147
Pearu <bartekg100@gmail.com>
2021-11-17 22:29:11 +0100

    logowanie ze zahashowanym hasłem

--- PHP/login.php
+++ PHP/login.php
@@ -6,7 +6,6 @@ $filePath = "..//HTML//$fileName.html";
 
 HtmlGenerator::ReadHTML($filePath);
 
-
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	if(isset($_POST['login'], $_POST['password']))
@@ -14,25 +13,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 		
 		$login = $_POST['login'];
 		$password = $_POST['password'];
+		$dbPassword = "";
 
 		require 'Config//db_login_data.php';
 
 		$conn = mysqli_connect($hostname, $user, $passwd, $dbName);
-		$sql = "SELECT user_id FROM accounts_data WHERE login='$login' AND password='$password'";
+		$sql = "SELECT user_id FROM accounts_data WHERE login='$login'";
+		$sql2 = "SELECT password FROM accounts_data WHERE login='$login'";
 
 		$result = $conn->query($sql);
+		$result2 = $conn->query($sql2);
 		
-		if(mysqli_num_rows($result) == 0)
+		if($result2->num_rows == 0)
 		{
 			echo '<script type="text/javascript">';
-			echo 'alert("Wprowadź poprawne dane.")';
+			echo 'alert("Wprowadź poprawny login!")';
 			echo '</script>';
 			$result->free_result();
 		}
 		else
 		{
-			while ($row = $result->fetch_assoc()) 
+			while($row2 = $result2->fetch_assoc())
 			{
+				$dbPassword = $row2['password'];
+				
+			}
+			if(password_verify($password, $dbPassword))
+			{
+				while ($row = $result->fetch_assoc()) 
+				{
 				session_start();
 				$_SESSION['userID'] = $row['user_id'];
 				echo $_SESSION['userID'];
@@ -40,11 +49,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 				header("location: main.php");
     			exit;
 
+				}
 			}
+			else
+			{
+				echo '<script type="text/javascript">';
+				echo 'alert("Wprowadź poprawne hasło!")';
+				echo '</script>';
+			}
+			
 		}
 	}
 	
 
 }
+
+echo "</body>
+</html>";
 
 ?>
