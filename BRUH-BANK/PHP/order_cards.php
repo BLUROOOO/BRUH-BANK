7ef9951ebae2b1fd5da0b8ad0557bbb4bdc483dc
Pearu <59788292+PearuUu@users.noreply.github.com>
2021-11-18 14:55:26 +0100

    bruh

---
+++ BRUH-BANK/PHP/order_cards.php
@@ -0,0 +1,23 @@
+<?php
+    session_start();
+
+    $now = time();
+
+    if ($now > $_SESSION['expire']) 
+	{
+        session_destroy();
+        header("Location: session_expire.php");
+    }
+
+    
+    require 'Config//Import_HTML.php';
+    $fileName = basename($_SERVER['PHP_SELF']);
+    $fileName = str_replace(".php", "", $fileName);
+    $filePath = "..//HTML//$fileName.html";
+
+    HtmlGenerator::ReadHTML($filePath);
+
+    echo "</body>
+    </html>";
+
+?>
