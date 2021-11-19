7f3376b22d526d023d81c45521a58248bea5788f
Pearu <bartekg100@gmail.com>
2021-11-19 23:19:14 +0100

    baza danych w chmurze

--- PHP/Config/db_login_data.php
+++ PHP/Config/db_login_data.php
@@ -1,6 +1,6 @@
 <?php
-	$hostname = "localhost";
-	$user = "root";
-	$passwd = "";
-	$dbName = "bank";
+	$hostname = "bbeu924kp2nhuacdbohv-mysql.services.clever-cloud.com";
+	$user = "ugt8yj3woqb7lkhm";
+	$passwd = "3ZBYhbl07r1aneu61MM4";
+	$dbName = "bbeu924kp2nhuacdbohv";
 ?>
